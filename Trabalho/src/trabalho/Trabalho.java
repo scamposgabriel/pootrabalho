9433229b0fd81b18fab2eb14e7b223d2f11affd6
Gabriel Campos <71159083+NabuDev@users.noreply.github.com>
2023-05-12 18:22:04 -0300

    Testando commit em casa

--- Trabalho/src/trabalho/Trabalho.java
+++ Trabalho/src/trabalho/Trabalho.java
@@ -14,7 +14,7 @@ public class Trabalho {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // apaguei para testar
     }
     
 }
